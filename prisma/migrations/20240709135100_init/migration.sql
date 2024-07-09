b0c74d16f9f2934cb196bc28f1a6f070f0903630
Saigulve2004 <saigulve9996@gmail.com>
2024-07-09 20:43:31 +0530

    made branche

---
+++ prisma/migrations/20240709135100_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "username" TEXT,
+    "content" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,30 @@
-<h1>Welcome to Summer Internship 2024</h1>
+<header class="bg-white py-4 shadow-md sticky top-10 z-20">
+    <div class="container mx-auto px-4 flex justify-between items-center">
+      <h1 class="font-bold text-2xl font-['Open Sans', sans-serif]">Sai's Instagram</h1>
+      <a href="/addpost" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "> Add Post</a>  
+  </header>
+  
+  <div class="container mx-auto md:my-5">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div>
+        <img class="w-full" src="https://picsum.photos/600" alt="Posts">
+        <div class="text-sm py-2 ">
+          <span class="font-bold"> Username</span>
+          <span class="text-grey-700 ">Caption </span>
+        </div>
+      </div>
+      <div>  <img class="w-full" src="https://picsum.photos/600" alt="Posts">
+        <div class="text-sm py-2 ">
+          <span class="font-bold"> Username</span>
+          <span class="text-grey-700 ">Caption </span>
+        </div>
+      </div>
+      <div>  <img class="w-full" src="https://picsum.photos/600" alt="Posts">
+        <div class="text-sm py-2 ">
+          <span class="font-bold"> Username</span>
+          <span class="text-grey-700 ">Caption </span>
+        </div>
+      </div>
+    </div>
+  </div>
+  
